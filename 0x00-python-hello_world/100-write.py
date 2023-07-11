c54e7c339e169a2d8c0a946483e1427022a774a1
caramelinvestor <caramelinvestor@gmail.com>
2023-07-11 10:41:31 -0700

    11. Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,28 +1,5 @@
-#include "lists.h"
-/**
- * check_cycle - tortoise and hare, but we need to find the loop
- * @head: Pointer to the head node
- * Return: 1 or 0
- */
-int check_cycle(listint_t *head)
-{
-        listint_t *slow = head, *fast = head;
-
-        while (slow && fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast)
-            {
-                slow = head;
-                while (slow && fast)
-                {
-                    if (slow == fast)
-                    return (1);
-                    slow = slow->next;
-                    fast = fast->next;
-                    }
-                }
-            }
-        return (0);
-}
+#!/usr/bin/python3
+import sys
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(message + '\n')
+sys.exit(1)
