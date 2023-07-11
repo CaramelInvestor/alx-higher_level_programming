4cce33346eaf26132f1617094293d608ddde027b
caramelinvestor <caramelinvestor@gmail.com>
2023-07-11 15:00:53 -0700

    11. a ^ b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        j = i - 32
-    else:
-        j = i
-    print("{}".format(chr(j)), end="")
+def pow(a, b):
+    return a ** b
