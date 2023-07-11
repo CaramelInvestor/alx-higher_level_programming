f11abd7842702703687dcd1601c61b4d94d51b30
caramelinvestor <caramelinvestor@gmail.com>
2023-07-11 15:29:06 -0700

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            if i == 100:
-                print("Buzz", end=" ")
-            else:
-                print("Buzz", end=" ")
-        else:
-            print("{0}".format(i), end=" ")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        j = i - 32
+    else:
+        j = i
+    print("{}".format(chr(j)), end="")
