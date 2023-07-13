8e8e67abc679b093401819b585a981368e622e36
caramelinvestor <caramelinvestor@gmail.com>
2023-07-13 06:00:34 -0700

    1. My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     result_add = add(a, b)
     result_sub = sub(a, b)
     result_mul = mul(a, b)
-    result_div = mul(a, b)
+    result_div = div(a, b)
 
     print("{} + {} = {}".format(a, b, result_add))
     print("{} - {} = {}".format(a, b, result_sub))
