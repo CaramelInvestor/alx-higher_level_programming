a35c3984e165469968af259baad4d4f11aaef89b
caramelinvestor <caramelinvestor@gmail.com>
2023-07-13 07:40:03 -0700

    5. Everything can be imported

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    character = chr(length)
+    for i in range(character):
+        if len(sys.argv[0]) != __:
+            print('{}'.format(character))
