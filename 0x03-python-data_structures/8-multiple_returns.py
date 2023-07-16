3597f6f746e28da2b590e254ff74732db9a690c0
caramelinvestor <caramelinvestor@gmail.com>
2023-07-16 14:03:02 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return(len(sentence), None)
-    first = sentence[0]
-    length = len(sentence)
-    return (length, first)
+        t = len(sentence), None
+        return t
+    t = len(sentence), sentence[0]
+    return t
