1d81573a9a09dbefd149b2b52dc153569697f6eb
caramelinvestor <caramelinvestor@gmail.com>
2023-07-16 13:58:22 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    '''returns a tuple containing the
-    length and first letter of the sentence'''
     if len(sentence) == 0:
         return(0, None)
     first = sentence[0]
