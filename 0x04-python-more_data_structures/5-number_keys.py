06035f6550571e639ac0d876dbe9b09f735a0490
caramelinvestor <caramelinvestor@gmail.com>
2023-07-19 06:52:39 -0700

    5. Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,6 +3,6 @@ def number_keys(a_dictionary):
     if a_dictionary is not None:
         # return len(a_dictionary.keys())
         sum = 0
-        for keys in a_dictionary:
+        for keys in a_dictionary.keys():
             sum += 1
         return sum
