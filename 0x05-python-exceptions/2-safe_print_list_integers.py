7a8eb387e9309181dc35ff1f8c1d046363475870
caramelinvestor <caramelinvestor@gmail.com>
2023-08-01 16:23:41 -0700

    2. Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -18,15 +18,14 @@ You are not allowed to use len()'''
 def safe_print_list_integers(my_list=[], x=0):
     '''A function that prints the first x elements
     of a list and only integers.'''
-    idx = 0
     printed_integer = 0
-    while printed_integer < x:
+    for idx in range(x):
         try:
             if isinstance(my_list[idx], int):
                 print("{:d}".format(my_list[idx]), end="")
                 printed_integer += 1
         except IndexError:
             break
-        idx += 1
+
     print()
     return printed_integer
