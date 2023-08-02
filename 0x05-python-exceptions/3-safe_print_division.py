428276091e96f92af11f1cc799ca0f7d6a555fe8
caramelinvestor <caramelinvestor@gmail.com>
2023-08-01 17:25:58 -0700

    push

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
+    """Returns the division of a by b."""
     try:
-        result = a / b
+        div = a / b
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
