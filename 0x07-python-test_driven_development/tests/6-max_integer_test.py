0cc4bba51b0cf2e89c287aa5d2a0950abe7e6ac2
caramelinvestor <caramelinvestor@gmail.com>
2023-08-11 20:08:30 -0700

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -24,14 +24,35 @@ class TestMaxInteger(TestCase):
         self.assertEqual(max_integer(ordered_ls), 6)
         self.assertEqual(max_integer(unordered_ls), 8)
 
-    def test_non_list(self):
-        """This module checks for non lists
-        """
-        self.assertTrue(max_integer([]))  # Test an empty list
-        self.assertTrue(max_integer([1, 2, 3]))  # Test a non-empty list
-        self.assertFalse(max_integer("not a list"))  # Test a string
-        self.assertFalse(max_integer(42))  # Test an int
-        self.assertFalse(max_integer({"key": "value"}))  # Test a dictionary
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
+    def test_one_element_list(self):
+        """Test a list with a single element."""
+        one_element = [7]
+        self.assertEqual(max_integer(one_element), 7)
+
+    def test_floats(self):
+        """Test a list of floats."""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
+
+    def test_ints_and_floats(self):
+        """Test a list of ints and floats."""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        """Test a string."""
+        string = "Brennan"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        """Test a list of strings."""
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
 
 
 if __name__ == '__main__':
