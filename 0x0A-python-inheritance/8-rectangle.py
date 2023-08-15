cd57912e9478bb849bc7d5ea45070846ed27ecaa
caramelinvestor <caramelinvestor@gmail.com>
2023-08-14 19:22:58 -0700

    class Rectangle that inherits from BaseGeometry (7-base_geometry.py)

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ This module inherits a class of BaseGeometry (7-base_geometry.py)
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     This class is an inherent class of BaseGeomtry
