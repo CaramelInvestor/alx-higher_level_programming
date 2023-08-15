61317353491472bf0b107f946e0ee84b1912b00a
caramelinvestor <caramelinvestor@gmail.com>
2023-08-15 10:26:31 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that defines a student
+"""
+
+
+class Student:
+    """
+    A student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        public instance attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
