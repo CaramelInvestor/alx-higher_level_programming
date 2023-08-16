a40898d418470bbedf96260b26a407fbf2e17f58
caramelinvestor <caramelinvestor@gmail.com>
2023-08-16 07:48:47 -0700

    Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module inherits a class of Rectangle (9.rectangle.py)
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class is an inherent class of rectangle
+    """
+    def __init__(self, size):
+        """
+        Instantiation of private attributes checks if size
+        is a positive integer
+        """
+        self.___size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """
+        print and str
+        """
+        return ("[Square]" + " " + str(self.__size) + "/" + str(self.__size))
