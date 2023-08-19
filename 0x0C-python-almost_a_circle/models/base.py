8a5d93c7642944044bb060a8a6cff44a59d6a182
caramelinvestor <caramelinvestor@gmail.com>
2023-08-18 17:13:29 -0700

    Task 1 to 3 done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,18 +5,17 @@ module to define a base class
 
 
 class Base:
-	"""
-	This is the superclass
-	"""
-	__nb_objects = 0
+    """
+    This is the superclass
+    """
+    __nb_objects = 0
 
-	def __init__(self, id=None):
-		"""
-		Instantiation method for id
-		"""
-		if id is not None and isinstance(id, int):
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
-	
+    def __init__(self, id=None):
+        """
+        Instantiation method for id
+        """
+        if id is not None and isinstance(id, int):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,89 +8,89 @@ from models.base import Base
 
 
 class Rectangle(Base):
-	"""
-	subclass that inherits from the base
-	superclass
-	"""
-	def __init__(self, width, height, x=0, y=0, id=None):
-		"""
-		Instantiation method to declare private attributes
-		"""
-		super().__init__(id)
-		self.width = width
-		self.height = height
-		self.x = x
-		self.y = y
+    """
+    subclass that inherits from the base
+    superclass
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Instantiation method to declare private attributes
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-	@property
-	def width(self):
-		"""
-		getter method for private width attribute
-		"""
-		return self.__width
+    @property
+    def width(self):
+        """
+        getter method for private width attribute
+        """
+        return self.__width
 
-	@width.setter
-	def width(self, value):
-		"""
-		setter method for private width attribute
-		"""
-		self.validator("width", value)
-		self.__width = value
+    @width.setter
+    def width(self, value):
+        """
+        setter method for private width attribute
+        """
+        self.validator("width", value)
+        self.__width = value
 
-	@property
-	def height(self):
-                """
-                getter method for private height attribute
-                """
-                return self.__height
+    @property
+    def height(self):
+        """
+        getter method for private height attribute
+        """
+        return self.__height
 
-	@height.setter
-	def height(self, value):
-                """
-                setter method for private height attribute
-                """
-	self.validator("height", value)
+    @height.setter
+    def height(self, value):
+        """
+        setter method for private height attribute
+        """
+        self.validator("height", value)
         self.__height = value
 
-	@property
-	def x(self):
-                """
-		getter method for private x attribute
-                """
-                return self.__x
+    @property
+    def x(self):
+        """
+        getter method for private x attribute
+        """
+        return self.__x
 
-	@x.setter
-	def x(self, value):
-                """
-                setter method for private x attribute
-                """
-		self.validator("x", value)
-		self.__x = value
+    @x.setter
+    def x(self, value):
+        """
+        setter method for private x attribute
+        """
+        self.validator("x", value)
+        self.__x = value
 
-	@property
-	def y(self):
-                """
-                getter method for private y attribute
-                """
-                return self.__y
+    @property
+    def y(self):
+        """
+        getter method for private y attribute
+        """
+        return self.__y
 
-	@y.setter
-	def y(self, value):
-                """
-                setter method for private y attribute
-                """
-		self.validator("y", value)
-                self.__y = value
+    @y.setter
+    def y(self, value):
+        """
+        setter method for private y attribute
+        """
+        self.validator("y", value)
+        self.__y = value
 
-	@staticmethod
-	def validator(attribute, value):
-		"""
-		method that validates all setter methods
-		"""
-		if type(value) != int:
-			raise TypeError("{} must be an integer".format(attribute))
-		if attribute == "weight" or attribute == "height":
-			if value <= 0:
-				raise ValueError("{} must be > 0".format(attribute))
-		elif value < 0:
-			raise ValueError("{} must be >= 0".format(attribute))
+    @staticmethod
+    def validator(attribute, value):
+        """
+        method that validates all setter methods
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "weight" or attribute == "height":
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(attribute))
+        elif value < 0:
+            raise ValueError("{} must be >= 0".format(attribute))
