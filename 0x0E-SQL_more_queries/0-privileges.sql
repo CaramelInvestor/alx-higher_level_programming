d48be688d18c7e22e2b8e2dba6376efb8ee69c9d
caramelinvestor <caramelinvestor@gmail.com>
2023-09-20 13:02:46 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- this script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
 
-GRANT PRIVILEGE ON database.table TO 'user_0d_1'@'localhost';
-GRANT PRIVILEGE ON database.table TO 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
