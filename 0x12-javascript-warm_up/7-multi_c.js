aea82572490f85818334dfed0eba60ba87b8d8c4
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-10-16 20:10:43 +0100

    Update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0 || isNaN(args[0])) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = parseInt(args[0], 10)
-  let i = 0;
 
-  while (i < x) {
-  console.log('C is fun');
-  i++;
+let num = process.argv[2];
+
+if (!isNaN(parseInt(process.argv[2]))) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
-}
+
+// const args = process.argv[2];
+// if (args.length === 0 || isNaN(args[0])) {
+//   console.log('Missing number of occurrences');
+// } else {
+//   const x = parseInt(args[0], 10)
+//   let i = 0;
+
+//   while (i < x) {
+//   console.log('C is fun');
+//   i++;
+// }
+// }
