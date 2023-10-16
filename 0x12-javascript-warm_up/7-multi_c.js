fae4b551e64a475a9c4ed69dccea9f141213e5d2
CaramelInvestor <128856519+CaramelInvestor@users.noreply.github.com>
2023-10-17 00:48:53 +0100

    Update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const args = process.argv[2];
-if (args.length === 0 || isNaN(parseInt(args[0]))) {
+const number = parseInt(args);
+
+if (isNaN(number)) {
   console.log('Missing number of occurrences');
 } else {
-  const x = parseInt(args[0]);
   let i = 0;
 
-  while (i < x) {
+  while (i < number) {
     console.log('C is fun');
     i++;
   }
