45bdee7d18a78e842d5edf8eb4ebb82ff0fb4d83
CaramelInvestor <128856519+CaramelInvestor@users.noreply.github.com>
2023-10-17 01:04:33 +0100

    Update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject = {
-  type: 'object',
-  value: 12,
-  incr: function () {
-    this.value += 1;
-  }
-};
+myObject.incr = function () {
+  this.value += 1;
+}
 
 myObject.incr();
 console.log(myObject);
