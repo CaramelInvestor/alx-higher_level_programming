5473cc2f378101bb0925ae253d1b778feedde11b
caramelinvestor <caramelinvestor@gmail.com>
2023-10-22 06:25:11 -0700

    SQL-ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""This script ists all states from the database hbtn_0e_0_usa"""
-
-import MySQLAlchemy
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states")  # SQL command to grab states from db
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
+    db.close()
