2c0248827a29074e2ca83edaf069f22bfeaf528c
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-02 16:42:30 +0100

    Update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk '{if ($0 == 200) {next} else {getline; print;}}'
+curl -Ls "$1"
