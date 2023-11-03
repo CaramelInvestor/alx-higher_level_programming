32ea9948b26bc544186678d568d3fa0427e5a27f
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-03 17:49:54 +0100

    Update

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pthon3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8)"""
+
+import sys
+from urllib import request, parse, urlopen
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {"email": sys.argv[2]}
+    data = parse.urlencode(data)
+    data = data.encode('ascii')
+
+    req = request.Request(url, data)
+    with urlopen(req) as response:
+        print(response.read().decode('utf-8'))
