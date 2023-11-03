3b7ad882b1e10929b1204e1cabeef1dcbb327764
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-03 22:38:33 +0100

    Update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-from urllib import request
+import requests
 
 if __name__ == "__main__":
-    req = request.get('https://alx-intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     t = req.text
     print("Body response:")
     print(f"\t- type: {type(t)}")
