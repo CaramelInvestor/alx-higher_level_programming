f21013afa3af846e3f27f5ed12e8cd607e78e953
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-03 18:19:02 +0100

    Handling Error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8)"""
+
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = request.Request(url)
+
+    try:
+        with request.urlopen(req) as response:
+            body = response.read().decode('utf-8') 
+            print(body)
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
