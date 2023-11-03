e49b2bee6376aa35fe44110bcd46926262db8f20
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-03 21:16:38 +0100

    Display header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = request.Request(url)
+    with request.urlopen(request) as response:
+        header = response.info()
+        print(header.get('X-Request-Id'))
