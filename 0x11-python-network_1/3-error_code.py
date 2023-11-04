a3953a2432c34618eb4c0310446027b9f648fcf4
UgonmaNkwocha <caramelinvestor@gmail.com>
2023-11-04 03:08:46 +0100

    Update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     try:
-        with request.urlopen(url) as response:
-            body = response.read() 
-            print(body.decode('utf-8'))
+        with request.urlopen(url) as res:
+            print(res.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code: {}".format(e.code))
